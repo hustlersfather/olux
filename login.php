2b52429704426dddc0158560061f1a89efe851cc
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-01-09 23:03:15 +0100

    Update login.php

--- login.php
+++ login.php
@@ -25,7 +25,78 @@
 <meta charset="utf-8">
 <title>Jerux SHOP</title>
 <style>
+/* Start Container */
+body {
+  font-family: "Lato", sans-serif;
+}
 
+
+.page-container {
+  border-radius: 10px;
+  box-shadow: 0px 0px 17.55px 9.45px rgba(0, 0, 0, 0.05);
+  width: 90%;
+  height: auto;
+  padding: 50px 0 0;
+  margin: auto;
+  border: 0;
+  margin-bottom: 40px;
+}
+
+/* End Container */
+
+/* Start title */
+.welcome-title {
+  color: #151515;
+
+  margin-bottom: auto;
+  font-weight: bold;
+}
+.card-heading p {
+  color: #555555;
+  margin-bottom: 0;
+}
+/* end  title */
+
+/* Start form style */
+form {
+  width: 85%;
+  margin: auto;
+}
+
+label {
+  color: #3d3d3d;
+  margin-bottom: 0.2rem;
+}
+.form-control {
+  height: 50px;
+}
+.form-control:focus {
+  box-shadow: none;
+}
+
+.form-group p {
+  font-size: 14px;
+  color: #555555;
+}
+.form-group a {
+  font-size: 14px;
+}
+/* login btn stle */
+.form-group .btn {
+  background-color: #0081ff;
+  color: #fff;
+  height: 50px;
+  font-weight: bold;
+}
+
+.input-group-text {
+  background-color: #fff;
+}
+
+.login-panel {
+	padding-top: 40px;
+}
+/* end btn stle */
 </style>
 
 </head>
