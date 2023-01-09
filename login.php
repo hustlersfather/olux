58909f6176cafebf83c4ae78e3fc389ee2050ec8
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-01-09 23:27:02 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,3 @@
-<?php
-  ob_start();
-  session_start();
-  include "includes/config.php";
-
-
-  date_default_timezone_set('UTC');
-
-
-  if(isset($_SESSION['sname']) and isset($_SESSION['spass'])){
-   header("location: index.html");
-   exit();
-}
-?>
 
 <?php include "loginpage1.php"; ?>
 
