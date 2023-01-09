a0e7f175aafbe25bb3abd2a76257909cc4569b7f
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-01-09 22:28:44 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<?php
+j<?php
   ob_start();
   session_start();
   include "includes/config.php";
@@ -92,29 +92,5 @@ $(window).on('load', function() {
 
 
 <script type="text/javascript">
-function logindiv(n,y,z,x){
-    $("#logindiv").html('<h4><img src="files/img/load.gif"></h4>').show();
-    $.ajax({
-    type:       'GET',
-    url:        'log-inpage'+ n +'.html?x='+Math.random(),
-    success:    function(data)
-    {
-        
-        $("#logindiv").html(data).show();
-        var obj = { Title: y, Url: z };
-        if (x != 1) {
-          history.pushState(obj, obj.Title, obj.Url);
-        }
-        else{
-          history.replaceState(obj, obj.Title, obj.Url);
-        }
-        document.title = y;
-    }});
-}
-$(window).on("popstate", function(e) {
-    if (event.state) {
-        location.replace(document.location);
-    }
-});
 
 </script>
